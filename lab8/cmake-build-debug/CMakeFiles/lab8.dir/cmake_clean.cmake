f6370bf641ab346200dc0e30b21bb84582f139a0
anmolshl <anmolshl2@gmail.com>
2017-05-10 21:55:47 -0700

    Add files via upload
    Lab 8 complete

---
+++ lab8/cmake-build-debug/CMakeFiles/lab8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab8.dir/avl.cc.o"
+  "CMakeFiles/lab8.dir/Unit.cc.o"
+  "lab8.pdb"
+  "lab8.exe"
+  "liblab8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
